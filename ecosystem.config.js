dee318ae9679305fac4b96b64dbafd3f8a04c191
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-01-22 16:41:41 +0530

    pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    apps: [{
+      name: "nestjs linux transcoder",
+      script: "node_modules/next/dist/bin/next",
+      args: "dev"
+    }]
+  }
--- src/app/api/getmedia/route.js
+++ src/app/api/getmedia/route.js
@@ -9,8 +9,8 @@ export async function GET(req, res) {
         query:
           "SELECT * FROM  media  ORDER BY MediaUploadedTime DESC",
       }); 
-      const urls=await Promise.all(response1.map(element => getObjectUrl(thumbnail1location + element.MediaID +'_th1.jpg')));
-      const videoUrls=await Promise.all(response1.map(element => getObjectUrl(proxy1location + element.MediaID +'_proxy1.mp4')));
+      const urls=await Promise.all(response1?.map(element => getObjectUrl(thumbnail1location + element.MediaID +'_th1.jpg')));
+      const videoUrls=await Promise.all(response1?.map(element => getObjectUrl(proxy1location + element.MediaID +'_proxy1.mp4')));
     const response = new Response(JSON.stringify({media:response1, urls,videoUrls }));
     return response;
   }
